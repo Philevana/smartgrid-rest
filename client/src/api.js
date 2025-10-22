325c35d3ed42635125bf94f9cae51c6ecc787edc
sxf51 <sxf5166@163.com>
2025-10-22 16:17:33 +1100

    改成公网ip

--- client/src/api.js
+++ client/src/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
-//const API_BASE = process.env.VUE_APP_API_BASE || 'http://localhost:5000/api'
-const API_BASE = 'http://localhost:5000/api'
+const API_BASE = process.env.VUE_APP_API_BASE || 'http://localhost:5000/api'
+//const API_BASE = 'http://localhost:5000/api'
 
 const client = axios.create({
   baseURL: API_BASE,
